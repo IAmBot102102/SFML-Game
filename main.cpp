0b0a6a94b53ecd11cf0289048ec7a6bd1bf9fe98
Kyle Guarco <kyleguarco55@gmail.com>
2019-10-26 08:33:31 -0400

    Created the first working window!

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+#include "SFML/Window.hpp"
+
 int main(){
-    
+    using namespace sf;
+
+    Window window(VideoMode(800, 600), "SFML Game");
+    window.setActive(true);
+
+    window.display();
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+            if (event.type == Event::KeyPressed) window.close();
+    }
+
     return 0;
 }
